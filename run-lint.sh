5fcfaf2fe9af05eca050f1308511de470f431948
Anjaneyulu80 <35999278+Anjaneyulu80@users.noreply.github.com>
2025-10-12 20:44:33 +0100

    Update run-lint.sh

--- run-lint.sh
+++ run-lint.sh
@@ -5,6 +5,6 @@ echo "🔍 Step 1: Checking YAML syntax..."
 find . -type f \( -name "*.yaml" -o -name "*.yml" \) -exec yamllint {} +
 
 echo "🚀 Step 2: Running ansible-lint (with custom rule)..."
-ansible-lint -p --config-file ansible-lint.yml ansible/test.yml
+ansible-lint ansible-lint.yml ansible/test.yml
 
 echo "✅ All checks completed successfully."
